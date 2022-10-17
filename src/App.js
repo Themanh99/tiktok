113d51a0925beb581a46365af8951b3f7cb99106
Themanh99 <themanhchu99@gmail.com>
2022-10-17 19:26:58 +0700

    commit lan dau

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+function App() {
+  const [avatar , setAvatar] = useState()
+
+  useEffect(() =>{
+    return () => {
+      avatar && URL.revokeObjectURL(avatar.preview)
+    }
+  } , [avatar])
+  const handlePreviewAvatar = (e) => {
+    const file = e.target.files[0]
+
+    file.preview = URL.createObjectURL(file)
+    setAvatar(file)
+  }
+
+
+  return (
+    <div>
+      <input
+        type="file"
+        onChange={handlePreviewAvatar}
+      />
+      {avatar && (
+        <img src={avatar.preview} alt="" width="80%" />
+      )}
+    </div>
+  );
+}
+
+export default App;
