b29c46d7e933d5375294282d783157087b58c7d1
Themanh99 <themanhchu99@gmail.com>
2022-10-19 16:40:49 +0700

    Add bai tap Callback Memo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
 import React from 'react';
-import { useCallback } from 'react';
-import { useState } from 'react';
-import Content from './Content';
+import { useState , useMemo ,useRef } from 'react';
 
 function App() {
-  const [count , setCount] = useState(0)
+  const [name , setName] = useState('')
+  const [price , setPrice] = useState('')
+  const [products , setProducts] = useState([])
 
-  const handleIncrease = () => {
-    setCount(prevCount => prevCount + 1)
+  const nameRef = useRef()
+  const handleSubmit = () => {
+    setProducts([...products,{
+      name,
+      price: parseInt(price)
+    }])
+    setName('')
+    setPrice('')
+
+    nameRef.current.focus()
   }
- 
+
+  const total = useMemo(() => {
+    const result = products.reduce((result, prod) => {
+      return result + prod.price
+    },[products]) 
+      return result
+  })
+  
   return (
     <div style={{padding : '10px 32px'}}>
-      <Content onIncrease={handleIncrease}/>
-      <h1>{count}</h1>
+      <input
+        ref={nameRef}
+        value={name}
+        placeholder="Enter name ?"
+        onChange={e => setName(e.target.value)}
+      /><br/>
+      <input
+        value={price}
+        placeholder="Enter price ?"
+        onChange={e => setPrice(e.target.value)}
+      /><br/>
+      <button onClick={handleSubmit}>Add</button>
+      <br/>
+      Total : {total}
+      <ul>
+        {products.map((product,index) => (
+          <li key={index}>{product.name} - {product.price}</li>
+        ))}
+      </ul>
     </div>
   );
 }
