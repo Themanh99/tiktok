d51c4b3c4a7ab1dfa62bfd0ce8c2046611efb849
Themanh99 <themanhchu99@gmail.com>
2022-10-25 23:08:05 +0700

    add cau hinh scss

--- src/App.js
+++ src/App.js
@@ -1,52 +1,9 @@
 import React from 'react';
-import { useState, useMemo, useRef } from 'react';
 
 function App() {
-  const [name, setName] = useState('');
-  const [price, setPrice] = useState('');
-  const [products, setProducts] = useState([]);
-
-  const nameRef = useRef();
-  const handleSubmit = () => {
-    setProducts([
-      ...products,
-      {
-        name,
-        price: parseInt(price),
-      },
-    ]);
-    setName('');
-    setPrice('');
-
-    nameRef.current.focus();
-  };
-
-  const total = useMemo(() => {
-    const result = products.reduce(
-      (result, prod) => {
-        return result + prod.price;
-      },
-      [products],
-    );
-    return result;
-  });
-
   return (
-    <div style={{ padding: '10px 32px' }}>
-      <input ref={nameRef} value={name} placeholder="Enter name ?" onChange={(e) => setName(e.target.value)} />
-      <br />
-      <input value={price} placeholder="Enter price ?" onChange={(e) => setPrice(e.target.value)} />
-      <br />
-      <button onClick={handleSubmit}>Add</button>
-      <br />
-      Total : {total}
-      <ul>
-        {products.map((product, index) => (
-          <li key={index}>
-            {product.name} - {product.price}
-          </li>
-        ))}
-      </ul>
+    <div>
+      <h1>Xin chao anh em</h1>
     </div>
   );
 }
