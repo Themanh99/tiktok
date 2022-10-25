0c87f4563ce5f8a63b2cc859c03e98398621a291
Themanh99 <themanhchu99@gmail.com>
2022-10-25 22:18:25 +0700

    cau hinh prettier code

--- src/App.js
+++ src/App.js
@@ -1,49 +1,50 @@
 import React from 'react';
-import { useState , useMemo ,useRef } from 'react';
+import { useState, useMemo, useRef } from 'react';
 
 function App() {
-  const [name , setName] = useState('')
-  const [price , setPrice] = useState('')
-  const [products , setProducts] = useState([])
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [products, setProducts] = useState([]);
 
-  const nameRef = useRef()
+  const nameRef = useRef();
   const handleSubmit = () => {
-    setProducts([...products,{
-      name,
-      price: parseInt(price)
-    }])
-    setName('')
-    setPrice('')
+    setProducts([
+      ...products,
+      {
+        name,
+        price: parseInt(price),
+      },
+    ]);
+    setName('');
+    setPrice('');
 
-    nameRef.current.focus()
-  }
+    nameRef.current.focus();
+  };
 
   const total = useMemo(() => {
-    const result = products.reduce((result, prod) => {
-      return result + prod.price
-    },[products]) 
-      return result
-  })
-  
+    const result = products.reduce(
+      (result, prod) => {
+        return result + prod.price;
+      },
+      [products],
+    );
+    return result;
+  });
+
   return (
-    <div style={{padding : '10px 32px'}}>
-      <input
-        ref={nameRef}
-        value={name}
-        placeholder="Enter name ?"
-        onChange={e => setName(e.target.value)}
-      /><br/>
-      <input
-        value={price}
-        placeholder="Enter price ?"
-        onChange={e => setPrice(e.target.value)}
-      /><br/>
+    <div style={{ padding: '10px 32px' }}>
+      <input ref={nameRef} value={name} placeholder="Enter name ?" onChange={(e) => setName(e.target.value)} />
+      <br />
+      <input value={price} placeholder="Enter price ?" onChange={(e) => setPrice(e.target.value)} />
+      <br />
       <button onClick={handleSubmit}>Add</button>
-      <br/>
+      <br />
       Total : {total}
       <ul>
-        {products.map((product,index) => (
-          <li key={index}>{product.name} - {product.price}</li>
+        {products.map((product, index) => (
+          <li key={index}>
+            {product.name} - {product.price}
+          </li>
         ))}
       </ul>
     </div>
